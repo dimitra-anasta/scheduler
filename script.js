58bc602b329c591cad343e679f296fd3950852f0
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-08-15 22:54:46 -0400

    index and script work

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 var timeDisplayEl = $('#time-display');
 var projectDisplayEl = $('#project-display');
+var currentDayEl = $('#currentDay');
+var currentHour = moment().hour();
+var btn = $('.btn');
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }function displayTime() {
-  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-  timeDisplayEl.text(rightNow);
+    var rightNow = moment().format('dddd, MMMM Do');
+    currentDayEl.text(rightNow);
 }
+displayTime();
+
+
+function checkTimeBlock() {
+for (var i=6; i<19; i++){
+  var text = localStorage.getItem(i)
+  $("#"+i).val(text);
+  if (currentHour>i){
+    $('#'+i).addClass("past");
+  }
+  if (currentHour === i){
+    $('#'+i).addClass("present");
+  }
+  if (currentHour<i){
+    $('#'+i).addClass("future");
+  }
+}
+}
+checkTimeBlock();
+
+function saveNotes(){
+var id = $(this).attr("data-id");
+var text = $("#"+id);
+localStorage.setItem(id, text.val());
+}
+btn.on("click", saveNotes);
 
 
 
