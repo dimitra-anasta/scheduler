df341cf1067e78be099c1267b2686f3a5362e3a0
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-08-15 20:38:09 -0400

    created table

---
+++ script.js
@@ -0,0 +1,13 @@
+var timeDisplayEl = $('#time-display');
+var projectDisplayEl = $('#project-display');
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }function displayTime() {
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+
+
